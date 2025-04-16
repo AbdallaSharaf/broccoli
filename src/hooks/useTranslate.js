2045da8dcf76ecbe05d1d873b5c9620233fb3ca5
Abdalla <abdallahsharaf95@gmail.com>
2025-04-16 07:32:28 +0200

    Update useTranslate.js

--- src/hooks/useTranslate.js
+++ src/hooks/useTranslate.js
@@ -1,16 +1,16 @@
 "use client"
 import enTranslations from '../translations/en.json';
 import arTranslations from '../translations/ar.json'; // Import Arabic translations
-import getItemsFromLocalstorage from '@/libs/getItemsFromLocalstorage';
+// import getItemsFromLocalstorage from '@/libs/getItemsFromLocalstorage';
 
 export function useTranslations(scope) {
-  const locale = getItemsFromLocalstorage('locale');
   // Handle server-side case
   if (typeof window === 'undefined') {
     return (key) => key; // Fallback during SSR
   }
-
-
+  
+  let locale = "en";
+  
   // Select the correct translations based on locale
   const currentTranslations = locale === 'ar' ? arTranslations : enTranslations;
 
