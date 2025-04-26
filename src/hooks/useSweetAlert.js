a041246ed4f98635359cf608ffda06cd1e110c69
Abdalla <abdallahsharaf95@gmail.com>
2025-04-26 18:55:58 +0300

    commit

--- src/hooks/useSweetAlert.js
+++ src/hooks/useSweetAlert.js
@@ -1,27 +1,27 @@
 "use client";
-
 import Swal from "sweetalert2";
 
 const useSweetAlert = () => {
-  const creteAlert = (type, message) => {
+  const createAlert = (type, message) => {
     const Toast = Swal.mixin({
       toast: true,
       position: "bottom-start",
       showConfirmButton: false,
       timer: 3000,
       timerProgressBar: true,
+      escapeHtml: true, // Prevent HTML in messages
       didOpen: (toast) => {
         toast.onmouseenter = Swal.stopTimer;
         toast.onmouseleave = Swal.resumeTimer;
       },
     });
     Toast.fire({
-      customClass: "z-xxxl",
       icon: type,
       title: message,
+      html: undefined, // Ensure no HTML parsing
     });
   };
-  return creteAlert;
+  return createAlert;
 };
 
 export default useSweetAlert;
