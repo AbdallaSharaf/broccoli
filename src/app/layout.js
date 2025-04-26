b72cea49938ca1850d8fe3b8ca1293f5dfce1351
MouhanadAhmed <houdacris88@gmail.com>
2025-04-26 23:43:59 +0300

    update layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -42,32 +42,75 @@ export default function RootLayout({ children }) {
     window.fbq("init", PIXEL_ID);
     window.fbq("track", "PageView");
   }, []);
-  return (
-    <html
-      lang="en"
-      suppressHydrationWarning={true}
+//   return (
+//     <html
+//       lang="en"
+//       suppressHydrationWarning={true}
+//       className={`${rajdhani.variable} ${open_sans.variable}`}
+//     >
+
+//       <body className={open_sans.className}>
+//       {/* <script>
+// !function(f,b,e,v,n,t,s)
+// {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+// n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+// if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+// n.queue=[];t=b.createElement(e);t.async=!0;
+// t.src=v;s=b.getElementsByTagName(e)[0];
+// s.parentNode.insertBefore(t,s)}(window, document,'script',
+// 'https://connect.facebook.net/en_US/fbevents.js');
+// fbq('init', '1359273798966268');
+// fbq('track', 'PageView');
+// </script>
+// <noscript><img height="1" width="1" style="display:none"
+// src="https://www.facebook.com/tr?id=1359273798966268&ev=PageView&noscript=1"
+// /></noscript> */}
+
+// <Script
+//         id="facebook-pixel"
+//         strategy="afterInteractive"
+//         dangerouslySetInnerHTML={{
+//           __html: `
+//             !function(f,b,e,v,n,t,s)
+//             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+//             n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+//             if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+//             n.queue=[];t=b.createElement(e);t.async=!0;
+//             t.src=v;s=b.getElementsByTagName(e)[0];
+//             s.parentNode.insertBefore(t,s)}(window, document,'script',
+//             'https://connect.facebook.net/en_US/fbevents.js');
+//             fbq('init', '${PIXEL_ID}');
+//             fbq('track', 'PageView');
+//           `,
+//         }}
+//       />
+//             <noscript>
+//         <img
+//           height="1"
+//           width="1"
+//           style={{ display: 'none' }}
+//           src={`https://www.facebook.com/tr?id=1359273798966268&ev=PageView&noscript=1`}
+//         />
+//       </noscript>
+//         <Suspense fallback={<div></div>}>
+//           {children}
+//           <Script src="/plugins.js" />
+//           <Script
+//             src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCeeHDCOXmUMja1CFg96RbtyKgx381yoBU"
+//             async
+//           />
+//         </Suspense>
+//       </body>
+//     </html>
+//   );
+return (
+  <html lang="en" 
+  suppressHydrationWarning={true}
       className={`${rajdhani.variable} ${open_sans.variable}`}
-    >
-
-      <body className={open_sans.className}>
-      {/* <script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1359273798966268');
-fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=1359273798966268&ev=PageView&noscript=1"
-/></noscript> */}
-
-<Script
-        id="facebook-pixel"
+  >
+    <head>
+      <Script
+        id="fb-pixel"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
@@ -84,23 +127,17 @@ src="https://www.facebook.com/tr?id=1359273798966268&ev=PageView&noscript=1"
           `,
         }}
       />
-            <noscript>
-        <img
-          height="1"
-          width="1"
-          style={{ display: 'none' }}
-          src={`https://www.facebook.com/tr?id=1359273798966268&ev=PageView&noscript=1`}
-        />
-      </noscript>
-        <Suspense fallback={<div></div>}>
-          {children}
-          <Script src="/plugins.js" />
-          <Script
-            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCeeHDCOXmUMja1CFg96RbtyKgx381yoBU"
-            async
-          />
-        </Suspense>
+    </head>
+    <body className={open_sans.className}>
+              <Suspense fallback={<div></div>}>
+        {children}
+           <Script src="/plugins.js" />
+           <Script
+             src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCeeHDCOXmUMja1CFg96RbtyKgx381yoBU"
+             async
+           />
+         </Suspense>
       </body>
-    </html>
-  );
+  </html>
+);
 }
