7a71e44d0d78f7d551ddbaad1091532c2caf536d
Abdalla <abdallahsharaf95@gmail.com>
2025-05-08 17:18:42 +0300

    Update axiosInstance.js

--- src/libs/axiosInstance.js
+++ src/libs/axiosInstance.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'https://fruits-heaven-api.onrender.com/api/v1',
-  timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
   },
