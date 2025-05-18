698edb6cd80ab2e5fd959d9539b5d98034d747eb
Abdalla <abdallahsharaf95@gmail.com>
2025-05-18 03:16:32 +0300

    Update CartProduct.js

--- src/components/shared/cart/CartProduct.js
+++ src/components/shared/cart/CartProduct.js
@@ -109,14 +109,16 @@ const CartProduct = ({
       </td>
       <td className="cart-product-price">
   <span>
-    {(
-      priceAfterDiscount &&
-      !isNaN(Number(priceAfterDiscount)) &&
-      Number(priceAfterDiscount) > 0 &&
-      (!priceAfterExpiresAt || new Date(priceAfterExpiresAt) > new Date())
-        ? Number(priceAfterDiscount)
-        : price
-    ) * quantity} <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1124.14 1256.39" width="16" height="16">
+{Number(
+      (
+        priceAfterDiscount &&
+        !isNaN(Number(priceAfterDiscount)) &&
+        Number(priceAfterDiscount) > 0 &&
+        (!priceAfterExpiresAt || new Date(priceAfterExpiresAt) > new Date())
+          ? Number(priceAfterDiscount)
+          : price
+      ) * quantity
+    ).toFixed(2)} <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1124.14 1256.39" width="16" height="16">
   <path fill="#231f20" d="M699.62,1113.02h0c-20.06,44.48-33.32,92.75-38.4,143.37l424.51-90.24c20.06-44.47,33.31-92.75,38.4-143.37l-424.51,90.24Z"/>
   <path fill="#231f20" d="M1085.73,895.8c20.06-44.47,33.32-92.75,38.4-143.37l-330.68,70.33v-135.2l292.27-62.11c20.06-44.47,33.32-92.75,38.4-143.37l-330.68,70.27V66.13c-50.67,28.45-95.67,66.32-132.25,110.99v403.35l-132.25,28.11V0c-50.67,28.44-95.67,66.32-132.25,110.99v525.69l-295.91,62.88c-20.06,44.47-33.33,92.75-38.42,143.37l334.33-71.05v170.26l-358.3,76.14c-20.06,44.47-33.32,92.75-38.4,143.37l375.04-79.7c30.53-6.35,56.77-24.4,73.83-49.24l68.78-101.97v-.02c7.14-10.55,11.3-23.27,11.3-36.97v-149.98l132.25-28.11v270.4l424.53-90.28Z"/>
 </svg>
@@ -127,7 +129,7 @@ const CartProduct = ({
   Number(priceAfterDiscount) > 0 &&
   Number(priceAfterDiscount) !== price &&
   (!priceAfterExpiresAt || new Date(priceAfterExpiresAt) > new Date()) && (
-    <del>{price * quantity} <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1124.14 1256.39" width="16" height="16">
+    <del>{(price * quantity).toFixed(2)} <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1124.14 1256.39" width="16" height="16">
   <path fill="#231f20" d="M699.62,1113.02h0c-20.06,44.48-33.32,92.75-38.4,143.37l424.51-90.24c20.06-44.47,33.31-92.75,38.4-143.37l-424.51,90.24Z"/>
   <path fill="#231f20" d="M1085.73,895.8c20.06-44.47,33.32-92.75,38.4-143.37l-330.68,70.33v-135.2l292.27-62.11c20.06-44.47,33.32-92.75,38.4-143.37l-330.68,70.27V66.13c-50.67,28.45-95.67,66.32-132.25,110.99v403.35l-132.25,28.11V0c-50.67,28.44-95.67,66.32-132.25,110.99v525.69l-295.91,62.88c-20.06,44.47-33.33,92.75-38.42,143.37l334.33-71.05v170.26l-358.3,76.14c-20.06,44.47-33.32,92.75-38.4,143.37l375.04-79.7c30.53-6.35,56.77-24.4,73.83-49.24l68.78-101.97v-.02c7.14-10.55,11.3-23.27,11.3-36.97v-149.98l132.25-28.11v270.4l424.53-90.28Z"/>
 </svg></del>
@@ -142,13 +144,13 @@ const CartProduct = ({
             <div className="dec qtybutton">-</div>
             <input
               value={quantity}
-              type="number"
+              type="text"
               name="qtybutton"
               className="cart-plus-minus-box"
-              min="0.25"
-              step="0.25"
               onChange={(e) => {
                 const val = parseFloat(e.target.value);
+
+                // Check if it's a number and a multiple of 0.25
                 if (!isNaN(val) && val > 0 && (val * 100) % 25 === 0) {
                   setQuantity(val);
                   setIsUpdate(true);
