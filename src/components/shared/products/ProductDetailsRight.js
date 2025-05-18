a799c97be30f2d69cfa608312dc95792bd51e329
Abdalla <abdallahsharaf95@gmail.com>
2025-05-18 04:06:23 +0300

    commit

--- src/components/shared/products/ProductDetailsRight.js
+++ src/components/shared/products/ProductDetailsRight.js
@@ -12,6 +12,7 @@ import countCommentLength from "@/libs/countCommentLength";
 import modifyNumber from "@/libs/modifyNumber";
 import getTranslatedName from "@/libs/getTranslatedName";
 import { useTranslations } from "@/hooks/useTranslate";
+import modifyAmount from "@/libs/modifyAmount";
 const ProductDetailsRight = ({ product }) => {
   // destructure current product
   // console.log(product.category)
@@ -123,14 +124,14 @@ const ProductDetailsRight = ({ product }) => {
       {/* price */}
       <div className="product-price text-nowrap">
         <span>
-          {(
+          {Number((
             priceAfterDiscount &&
             !isNaN(Number(priceAfterDiscount)) &&
             Number(priceAfterDiscount) > 0 &&
             (!priceAfterExpiresAt || new Date(priceAfterExpiresAt) > new Date())
               ? Number(priceAfterDiscount)
               : price
-          ) * quantity} <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1124.14 1256.39" width="16" height="16">
+          ) * quantity).toFixed(2)} <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1124.14 1256.39" width="16" height="16">
   <path fill="#231f20" d="M699.62,1113.02h0c-20.06,44.48-33.32,92.75-38.4,143.37l424.51-90.24c20.06-44.47,33.31-92.75,38.4-143.37l-424.51,90.24Z"/>
   <path fill="#231f20" d="M1085.73,895.8c20.06-44.47,33.32-92.75,38.4-143.37l-330.68,70.33v-135.2l292.27-62.11c20.06-44.47,33.32-92.75,38.4-143.37l-330.68,70.27V66.13c-50.67,28.45-95.67,66.32-132.25,110.99v403.35l-132.25,28.11V0c-50.67,28.44-95.67,66.32-132.25,110.99v525.69l-295.91,62.88c-20.06,44.47-33.33,92.75-38.42,143.37l334.33-71.05v170.26l-358.3,76.14c-20.06,44.47-33.32,92.75-38.4,143.37l375.04-79.7c30.53-6.35,56.77-24.4,73.83-49.24l68.78-101.97v-.02c7.14-10.55,11.3-23.27,11.3-36.97v-149.98l132.25-28.11v270.4l424.53-90.28Z"/>
 </svg>
@@ -141,7 +142,7 @@ const ProductDetailsRight = ({ product }) => {
         Number(priceAfterDiscount) > 0 &&
         Number(priceAfterDiscount) !== price &&
         (!priceAfterExpiresAt || new Date(priceAfterExpiresAt) > new Date()) && (
-          <del>{price * quantity} <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1124.14 1256.39" width="16" height="16">
+          <del>{modifyAmount(price * quantity)} <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1124.14 1256.39" width="16" height="16">
   <path fill="#231f20" d="M699.62,1113.02h0c-20.06,44.48-33.32,92.75-38.4,143.37l424.51-90.24c20.06-44.47,33.31-92.75,38.4-143.37l-424.51,90.24Z"/>
   <path fill="#231f20" d="M1085.73,895.8c20.06-44.47,33.32-92.75,38.4-143.37l-330.68,70.33v-135.2l292.27-62.11c20.06-44.47,33.32-92.75,38.4-143.37l-330.68,70.27V66.13c-50.67,28.45-95.67,66.32-132.25,110.99v403.35l-132.25,28.11V0c-50.67,28.44-95.67,66.32-132.25,110.99v525.69l-295.91,62.88c-20.06,44.47-33.33,92.75-38.42,143.37l334.33-71.05v170.26l-358.3,76.14c-20.06,44.47-33.32,92.75-38.4,143.37l375.04-79.7c30.53-6.35,56.77-24.4,73.83-49.24l68.78-101.97v-.02c7.14-10.55,11.3-23.27,11.3-36.97v-149.98l132.25-28.11v270.4l424.53-90.28Z"/>
 </svg></del>
@@ -180,18 +181,20 @@ const ProductDetailsRight = ({ product }) => {
           <li>
             <div className="cart-plus-minus" ref={inputRef}>
             <div className="dec qtybutton">-</div>
-              <input
-                onChange={(e) =>{
-                  setQuantity(
-                    !Number(e.target.value) ? 0.25 : Number(e.target.value)
-                  )
+                       <input
+              value={quantity}
+              type="text"
+              name="qtybutton"
+              className="cart-plus-minus-box"
+              onChange={(e) => {
+                const val = parseFloat(e.target.value);
+
+                // Check if it's a number and a multiple of 0.25
+                if (!isNaN(val) && val > 0 && (val * 100) % 25 === 0) {
+                  setQuantity(val);
                 }
-              }
-                type="text"
-                value={quantity}
-                name="qtybutton"
-                className="cart-plus-minus-box"
-              />
+              }}
+            />
               <div className="inc qtybutton">+</div>
             </div>
           </li>{" "}
